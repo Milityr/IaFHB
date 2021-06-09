410a03db1c48b0b1ca193e9f7b56c9839dc206b1
Milityr <wakmiec@gmail.com>
2021-06-09 14:11:01 +0200

    Second commit

--- script.js
+++ script.js
@@ -1,57 +1,83 @@
-window.onload = function(){createSelect()};
+window.addEventListener('load', function () {
+    pagination(1)
+});
+window.addEventListener('load', function () {
+    createHouses(1)
+});
 const arr = new Object
-function createSelect(){
-    const select = document.createElement("select")
-    select.onchange = createHouses
-    select.id = "select"
-    document.body.appendChild(select)
-        for(j=1;j<=9;j++){
-            let option = document.createElement("option")
-            option.value = j
-            option.innerText = j
-            select.appendChild(option)
+pagination = (number) => {
+    const div = document.getElementById("pagination")
+    div.innerHTML = ""
+    for (j = 0; j <= 4; j++) {
+        const paginationElement = document.createElement("button")
+        paginationElement.innerText = number
+        if (j == 0) {
+            paginationElement.onclick = function () {
+                if (parseInt(this.innerText) <= 1) {
+                    pagination(1)
+                }
+                else {
+                    pagination(parseInt(this.innerText) - 1)
+                }
+                createHouses(this.innerText)
+            }
         }
-        createHouses()
-}
-function createHouses(){
-const div = document.getElementById("HousesDisplay")
-div.innerHTML = ""
-const CD = document.getElementById("CD")
-CD.innerHTML = ""
- fetch('https://www.anapioficeandfire.com/api/houses?page='+document.getElementById("select").value+'&pageSize=50')
-    .then(response => response.json())
-    .then(data => {
-        for(i=0;i<data.length;i++){
-        let hd = document.createElement("a");
-        hd.innerText = ""+data[i].name+""
-        hd.onclick = createCharacters
-        hd.id = i
-        div.appendChild(hd)
-        let br = document.createElement("br")
-        div.appendChild(br)
+        if (j > 0) {
+            paginationElement.onclick = function () {
+                if (parseInt(this.innerText) >= 41) {
+                    pagination(41)
+                }
+                else {
+                    pagination(parseInt(this.innerText))
+                }
+                createHouses(this.innerText)
+            }
+        }
+        div.appendChild(paginationElement)
+        number += 1
     }
-    arr.h = (data)
-    })}
-function createCharacters(){
- let character = arr.h[this.id].swornMembers
- CD.innerHTML = ""
- if(character.length == 0){
-    let hd = document.createElement("a");
-    hd.innerText = "This House is Empty"
-    CD.appendChild(hd)
- }
- else
- for(i=0;i<character.length;i++){
-    let hd = document.createElement("a");
-    fetch(character[i])
-    .then(response => response.json())
-    .then(char => {
-        hd.innerText = ""+char.name+""
-        CD.appendChild(hd)
-        let br = document.createElement("br")
-        CD.appendChild(br)
-    })}
-    
- }
+}
+createHouses = async (pageNumber) => {
+    const div = document.getElementById("HousesDisplay")
+    div.innerHTML = ""
+    const characterDisplay = document.getElementById("characterDisplay")
+    characterDisplay.innerHTML = ""
+    await fetch('https://www.anapioficeandfire.com/api/houses?page=' + pageNumber + '&pageSize=10')
+        .then(response => response.json())
+        .then(data => {
+            for (i = 0; i < data.length; i++) {
+                const houseDisplay = document.createElement("p");
+                houseDisplay.innerText = "" + data[i].name + ""
+                houseDisplay.onclick = createCharacters
+                houseDisplay.id = i
+                div.appendChild(houseDisplay)
+            }
+            arr.h = (data)
+        })
+        .catch((error) => {
+            document.body.innerHTML = '<h3>Sorry. An error has occured, connection with API impossible!</h3>'
+        });
+}
+function createCharacters() {
+    const character = arr.h[this.id].swornMembers
+    characterDisplay.innerHTML = ""
+    if (character.length == 0) {
+        const houseDisplay = document.createElement("p");
+        houseDisplay.innerText = "This House is Empty"
+        characterDisplay.appendChild(houseDisplay)
+    }
+    else
+        for (i = 0; i < character.length; i++) {
+            const houseDisplay = document.createElement("p");
+            fetch(character[i])
+                .then(response => response.json())
+                .then(char => {
+                    houseDisplay.innerText = "" + char.name + ""
+                    characterDisplay.appendChild(houseDisplay)
+                })
+                .catch((error) => {
+                    document.body.innerHTML = '<h3>Sorry. An error has occured, connection with API impossible!</h3>'
+                });
+        }
+}
 
-        
